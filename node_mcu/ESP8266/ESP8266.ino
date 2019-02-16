4123f4b9671c596c9a6557878b1bab35b11f8422
khacpv <khacpv@gmail.com>
2019-02-16 09:24:06 +0700

    update start script

--- node_mcu/ESP8266/ESP8266.ino
+++ node_mcu/ESP8266/ESP8266.ino
@@ -22,6 +22,7 @@ unsigned long lastsend = 0;
 String JSON;
 JsonObject &root = jsonBuffer.createObject();
 char ipStr[100];
+char messageStr[100];
 
 // LED
 int led_pin = 5;
@@ -53,6 +54,8 @@ void setup()
   {
     delay(500);
     Serial.print(".");
+    sprintf(messageStr,"connecting wifi!");
+    Nokia5110_print(messageStr);
   }
 
   Serial.println("");
@@ -61,21 +64,40 @@ void setup()
   Serial.println(WiFi.localIP());
 
   sprintf(ipStr, "%s", WiFi.localIP().toString().c_str());
-  Nokia5110_print(ssid, ipStr, "connected!");
+  sprintf(messageStr,"connected router!");
+  Nokia5110_print(ssid, ipStr, messageStr);
 
   if (!client.connect(host, port))
   {
     Serial.println("connection failed");
+    sprintf(messageStr,"connect server failed!");
+    Nokia5110_print(messageStr);
     return;
   }
   if (client.connected())
   {
     client.send("connection", "message", "Connected !!!!");
+    sprintf(messageStr,"connected server!");
+    Nokia5110_print(ssid, ipStr, messageStr);
   }
 }
 
 void loop()
 {
+  if (WiFi.status() != WL_CONNECTED){
+    sprintf(messageStr,"disconnected wifi!");
+    Nokia5110_print(messageStr);
+    delay(500);
+    return;
+  }
+  if (!client.connected())
+  {
+    client.send("connection", "message", "Connected !!!!");
+    sprintf(messageStr,"disconnected server!");
+    Nokia5110_print(ssid, ipStr, messageStr);
+    delay(500);
+    return;
+  }
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis > interval)
   {
@@ -86,7 +108,8 @@ void loop()
     client.send("atime", "message", "Time please?");
     client.sendJSON("JSON", JSON);
     Led_on(led_pin);
-
+    sprintf(messageStr,"sent %d", lastsend);
+    Nokia5110_print(ssid, ipStr, messageStr);
     lastsend = millis();
   }
   if (client.monitor())
@@ -97,6 +120,8 @@ void loop()
     {
       Serial.print("Il est ");
       Serial.println(Rcontent);
+      sprintf(messageStr,"msg: %s", Rcontent.c_str());
+      Nokia5110_print(ssid, ipStr, messageStr);
       Led_off(led_pin);
     }
   }
