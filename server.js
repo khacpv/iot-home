4c04f0b85f21cd79f2e48ea7e9ac5f20742b536e
khacpv <khacpv@gmail.com>
2019-02-16 09:29:55 +0700

    add port

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var util = require('util');
 var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
 var fs = require('fs');
-app.listen(3000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 function handler(req, res) {
   fs.readFile(__dirname + '/index.html', function(err, data) {
